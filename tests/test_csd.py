45446be33542954d2cbeb686db7fadf71b6370f3
Wai-Shing Luk <luk036@gmail.com>
2022-08-02 15:51:57 +0800

    remove quickcheck

--- tests/test_csd.py
+++ tests/test_csd.py
@@ -3,7 +3,6 @@ import pytest
 from csd_py.csd import to_csd, to_decimal
 
 
-@pytest.mark.randomize(d=int, ncalls=5)
-def test_csd(d):
-    f = float(d)
+def test_csd():
+    f = 1234.0
     assert f == to_decimal(to_csd(f, 2))
