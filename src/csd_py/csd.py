0382a7f10aef640512e9d49bae7e335c5153277a
Wai-Shing Luk <luk036@gmail.com>
2022-10-08 15:18:07 +0800

    variable name at least three characters

--- src/csd_py/csd.py
+++ src/csd_py/csd.py
@@ -34,30 +34,31 @@ def to_csd(num: float, places: int) -> str:
 
     absnum = fabs(num)
     if absnum < 1.0:
-        n = 0
-        csd_str = "0"
+        rem = 0
+        csd = "0"
     else:
-        n = ceil(log(absnum * 1.5, 2))
-        csd_str = ""
-    pow2n = pow(2, n)
-    while n > -places:
-        if n == 0:
-            csd_str += "."
+        rem = ceil(log(absnum * 1.5, 2))
+        csd = ""
+    pow2n = pow(2, rem)
+    while rem > -places:
+        if rem == 0:
+            csd += "."
         # convert the number
         pow2n_half = pow2n / 2
-        d = 3 * num
-        if d > pow2n:
-            csd_str += "+"
+        det = 3 * num
+        if det > pow2n:
+            csd += "+"
             num -= pow2n_half
-        elif d < -pow2n:
-            csd_str += "-"
+        elif det < -pow2n:
+            csd += "-"
             num += pow2n_half
         else:
-            csd_str += "0"
+            csd += "0"
         pow2n = pow2n_half
-        n -= 1
+        rem -= 1
 
-    return csd_str
+    return csd
+
 
 def to_csd_i(num: int) -> str:
     """Convert the argument `num` to a string in CSD Format.
@@ -82,28 +83,28 @@ def to_csd_i(num: int) -> str:
         return "0"
 
     absnum = abs(num)
-    n = ceil(log(absnum * 1.5, 2))
-    csd_str = ""
-    pow2n = pow(2, n)
-    while n > 0:
+    rem = ceil(log(absnum * 1.5, 2))
+    csd = ""
+    pow2n = pow(2, rem)
+    while rem > 0:
         # convert the number
         pow2n_half = pow2n / 2
-        d = 3 * num
-        if d > pow2n:
-            csd_str += "+"
+        det = 3 * num
+        if det > pow2n:
+            csd += "+"
             num -= pow2n_half
-        elif d < -pow2n:
-            csd_str += "-"
+        elif det < -pow2n:
+            csd += "-"
             num += pow2n_half
         else:
-            csd_str += "0"
+            csd += "0"
         pow2n = pow2n_half
-        n -= 1
+        rem -= 1
 
-    return csd_str
+    return csd
 
 
-def to_decimal(csd_str: str) -> float:
+def to_decimal(csd: str) -> float:
     """Convert the argument to a decimal number
 
     Original author: Harnesser
@@ -111,7 +112,7 @@ def to_decimal(csd_str: str) -> float:
     License: GPL2
 
     Args:
-        csd_str (str): string containing the CSD value
+        csd (str): string containing the CSD value
 
     Returns:
         float: decimal value of the CSD format
@@ -125,25 +126,25 @@ def to_decimal(csd_str: str) -> float:
 
     num: float = 0.0
     loc: int = 0
-    for i, c in enumerate(csd_str):
-        if c == "0":
+    for pos, digit in enumerate(csd):
+        if digit == "0":
             num *= 2.0
-        elif c == "+":
+        elif digit == "+":
             num = num * 2.0 + 1.0
-        elif c == "-":
+        elif digit == "-":
             num = num * 2.0 - 1.0
-        elif c == ".":
-            loc = i + 1
+        elif digit == ".":
+            loc = pos + 1
         else:
             pass  # ignore other characters
 
     if loc != 0:
-        num /= pow(2.0, len(csd_str) - loc)
+        num /= pow(2.0, len(csd) - loc)
 
     return num
 
 
-def to_decimal_i(csd_str: str) -> int:
+def to_decimal_i(csd: str) -> int:
     """Convert the argument to a decimal number
 
     Original author: Harnesser
@@ -151,7 +152,7 @@ def to_decimal_i(csd_str: str) -> int:
     License: GPL2
 
     Args:
-        csd_str (str): string containing the CSD value
+        csd (str): string containing the CSD value
 
     Returns:
         float: decimal value of the CSD format
@@ -161,12 +162,12 @@ def to_decimal_i(csd_str: str) -> int:
         28
     """
     num: int = 0
-    for c in csd_str:
-        if c == "0":
+    for digit in csd:
+        if digit == "0":
             num *= 2
-        elif c == "+":
+        elif digit == "+":
             num = num * 2 + 1
-        elif c == "-":
+        elif digit == "-":
             num = num * 2 - 1
         else:
             pass  # ignore other characters
@@ -199,32 +200,32 @@ def to_csdfixed(num: float, nnz: int) -> str:
 
     absnum = fabs(num)
     if absnum < 1.0:
-        n = 0
-        csd_str = "0"
+        rem = 0
+        csd = "0"
     else:
-        n = ceil(log(absnum * 1.5, 2))
-        csd_str = ""
-    pow2n = pow(2.0, n)
-    while n > 0 or (nnz > 0 and fabs(num) > 1e-100):
-        if n == 0:
-            csd_str += "."
+        rem = ceil(log(absnum * 1.5, 2))
+        csd = ""
+    pow2n = pow(2.0, rem)
+    while rem > 0 or (nnz > 0 and fabs(num) > 1e-100):
+        if rem == 0:
+            csd += "."
         pow2n_half = pow2n / 2
-        d = 3.0 * num
-        if d > pow2n:
-            csd_str += "+"
+        det = 3.0 * num
+        if det > pow2n:
+            csd += "+"
             num -= pow2n_half
             nnz -= 1
-        elif d < -pow2n:
-            csd_str += "-"
+        elif det < -pow2n:
+            csd += "-"
             num += pow2n_half
             nnz -= 1
         else:
-            csd_str += "0"
+            csd += "0"
         pow2n = pow2n_half
-        n -= 1
+        rem -= 1
         if nnz == 0:
             num = 0.0
-    return csd_str
+    return csd
 
 
 if __name__ == "__main__":
