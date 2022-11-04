d4d08e50c0ded9d4f73fbdd765e56e7bdd32944d
Wai-Shing Luk <luk036@gmail.com>
2022-11-04 16:25:13 +0800

    add hypothesis tests

--- tests/test_csd.py
+++ tests/test_csd.py
@@ -1,6 +1,17 @@
-from csd_py.csd import to_csd, to_decimal
+from math import sqrt
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
+from csd_py.csd import to_csd, to_csd_i, to_decimal, to_decimal_i
 
 
 def test_csd():
-    f = 1234.0
+    f = 1234.5
     assert f == to_decimal(to_csd(f, 2))
+
+
+@given(integers())
+def test_csd_i(number):
+    num = int(sqrt(abs(number)))  # don't too large
+    assert num == to_decimal_i(to_csd_i(num))
